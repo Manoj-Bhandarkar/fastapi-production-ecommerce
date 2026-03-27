47e19039344baf59e6564bef778a4c91935e1d8d
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-03-27 15:20:52 +0530

    Auth - User Register with Hashed Password - Service - create_user function created

---
+++ backend/src/account/services.py
@@ -0,0 +1,25 @@
+from src.account.models import User
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import Select
+from fastapi import HTTPException, status
+from src.account.schemas import UserCreate
+from src.account.utils import hash_password
+
+async def create_user(session: AsyncSession, user: UserCreate):
+    stmt = Select(User).where(User.email == user.email)
+    result = await session.scalar(stmt)
+
+    if result:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered"
+        )
+
+    new_user = User(
+        email=user.email, 
+        hashed_password=hash_password(user.password)
+        )
+
+    session.add(new_user)
+    await session.commit()
+    await session.refresh(new_user)
+    return new_user
