7daee90a4e7ca4534f4cd5d05f67665e1008176b
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-03-27 15:21:28 +0530

    Auth - User Register with Hashed Password - Router create and Register in main

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from src.account.routers import router as account_router
 
 app = FastAPI(title="FastAPI E-Commerce Backend")
 
 @app.get("/")
 def root():
     return {"message": "Welcome to the E-Commerce API"}
+
+app.include_router(account_router, prefix="/api/account", tags=["Account"])
