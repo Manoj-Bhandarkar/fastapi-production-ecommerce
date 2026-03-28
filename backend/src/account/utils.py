314d3ea94a0b4115b2b16befe2d8149e9cf45cae
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-03-28 09:49:25 +0530

    Auth - Create Token - Refresh Token and create token utility Fucntion

--- backend/src/account/utils.py
+++ backend/src/account/utils.py
@@ -30,3 +30,21 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     )
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, JWT_SECRET_KEY, JWT_ALGORITHM)
+
+
+async def create_tokens(session: AsyncSession, user: User):
+    access_token = create_access_token(data={"sub": str(user.id)})
+
+    refresh_token_str = str(uuid.uuid4())
+    expires_at = datetime.now(timezone.utc) + timedelta(days=JWT_REFRESH_TOKEN_TIME_DAY)
+
+    refresh_token = RefreshToken(
+        user_id=user.id, token=refresh_token_str, expires_at=expires_at
+    )
+    session.add(refresh_token)
+    await session.commit()
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
