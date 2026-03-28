2f83d5552cb4acebc73bd1cbe1bb6eefc5e46d22
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-03-28 09:48:12 +0530

    Auth - Create Access Token utility Fucntion

--- backend/src/account/routers.py
+++ backend/src/account/routers.py
@@ -2,15 +2,22 @@ from fastapi import APIRouter, HTTPException, status
 from src.db.config import SessionDep
 from src.account.schemas import UserCreate, UserOut, UserLogin
 from src.account.services import create_user, authenticate_user
+from src.account.utils import create_tokens
 
 router = APIRouter()
+
 
 @router.post("/register", response_model=UserOut)
 async def register(session: SessionDep, user: UserCreate):
     return await create_user(session, user)
 
+
 @router.post("/login")
 async def login(session: SessionDep, user_login: UserLogin):
     user = await authenticate_user(session, user_login)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials"
+        )
+
+    tokens = await create_tokens(session, user)
--- backend/src/account/utils.py
+++ backend/src/account/utils.py
@@ -1,6 +1,19 @@
 from passlib.context import CryptContext
+from datetime import timedelta, datetime, timezone
+from decouple import config
+from jose import jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.account.models import User, RefreshToken
+from sqlalchemy.ext.asyncio import AsyncSession
+import uuid
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+JWT_ACCESS_TOKEN_TIME_MIN = config("JWT_ACCESS_TOKEN_TIME_MIN")
+JWT_ALGORITHM = config("JWT_ALGORITHM")
+JWT_SECRET_KEY = config("JWT_SECRET_KEY")
+JWT_REFRESH_TOKEN_TIME_DAY = config("JWT_REFRESH_TOKEN_TIME_DAY")
+
 
 def hash_password(password: str):
     return pwd_context.hash(password)
@@ -8,3 +21,12 @@ def hash_password(password: str):
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify_password(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (
+        expires_delta or timedelta(minutes=JWT_ACCESS_TOKEN_TIME_MIN)
+    )
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, JWT_SECRET_KEY, JWT_ALGORITHM)
