b7679f8e22dfc87d3713223090cb6759d9fff69a
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-05 07:57:02 +0530

    Product -  Search Product - services is created

--- backend/src/product/services.py
+++ backend/src/product/services.py
@@ -85,3 +85,50 @@ async def get_product_by_slug(session: AsyncSession, slug: str) -> ProductOut | 
   stmt = select(Product).options(selectinload(Product.categories)).where(Product.slug == slug)
   result = await session.execute(stmt)
   return result.scalar()
+
+async def search_products(
+    session: AsyncSession,
+    category_names: list[str] | None = None,
+    title: str | None = None,
+    description: str | None = None,
+    min_price: float | None = None,
+    max_price: float | None = None,
+    limit: int = 5,
+    page: int = 1
+) -> dict:
+  stmt = select(Product).options(selectinload(Product.categories))
+
+  if category_names:
+    stmt = stmt.join(Product.categories).where(Category.name.in_(category_names)).distinct()
+
+  filters = []
+
+  if title:
+    filters.append(Product.title.like(f"%{title}%"))
+
+  if description:
+    filters.append(Product.description.like(f"%{description}%"))
+
+  if min_price is not None:
+    filters.append(Product.price >= min_price)
+
+  if max_price is not None:
+    filters.append(Product.price <= max_price)
+
+  if filters:
+    stmt = stmt.where(and_(*filters))
+
+  count_stmt = stmt.with_only_columns(func.count(Product.id)).order_by(None)
+  total = await session.scalar(count_stmt)
+
+  stmt = stmt.limit(limit).offset((page-1)*limit)
+
+  result = await session.execute(stmt)
+  products = result.scalars().all()
+
+  return {
+    "total": total,
+    "page": page,
+    "limit": limit,
+    "items": products
+  }
