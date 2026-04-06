3077bd75a4d56bc156d5cec1774fb20f7a496735
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-06 22:26:29 +0530

    Product - Product Update - update_product_by_id route created

--- backend/src/product/services.py
+++ backend/src/product/services.py
@@ -3,24 +3,35 @@ from sqlalchemy import select, func, and_
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.product.utils import generate_slug, save_upload_file
-from src.product.schemas import CategoryCreate, CategoryOut, ProductCreate, ProductOut
+from src.product.schemas import (
+    CategoryCreate,
+    CategoryOut,
+    ProductCreate,
+    ProductOut,
+    ProductUpdate,
+)
 from src.product.models import Category, Product
 from sqlalchemy.orm import selectinload
 
+
 ################# Category ###################
-async def create_category(session: AsyncSession, category: CategoryCreate)->CategoryOut:
+async def create_category(
+    session: AsyncSession, category: CategoryCreate
+) -> CategoryOut:
     category = Category(name=category.name)
     session.add(category)
     await session.commit()
     await session.refresh(category)
     return category
 
-async def get_all_category(session:AsyncSession) -> list[CategoryOut]:
+
+async def get_all_category(session: AsyncSession) -> list[CategoryOut]:
     stmt = select(Category).order_by(Category.id)
     result = await session.scalars(stmt)
     return result.all()
 
-async def delete_category(session:AsyncSession, category_id:int)-> bool:
+
+async def delete_category(session: AsyncSession, category_id: int) -> bool:
     category = await session.get(Category, category_id)
     if not category:
         return False
@@ -28,63 +39,72 @@ async def delete_category(session:AsyncSession, category_id:int)-> bool:
     await session.commit()
     return True
 
+
 ################ Product ##################
 
+
 async def create_product(
-    session: AsyncSession, 
-    data: ProductCreate, 
-    image_url: UploadFile | None = None
-  ) -> Product:
-  if data.stock_quantity < 0:
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Stock quantity cannot be negative")
-  
-  image_path = await save_upload_file(image_url, "images")
+    session: AsyncSession, data: ProductCreate, image_url: UploadFile | None = None
+) -> Product:
+    if data.stock_quantity < 0:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Stock quantity cannot be negative",
+        )
 
-  categories = []
-  if data.category_ids:
-    category_stmt = select(Category).where(Category.id.in_(data.category_ids))
-    category_result = await session.execute(category_stmt)
-    categories = category_result.scalars().all()
-  
-  product_dict = data.model_dump(exclude={"category_ids"})
-  if not product_dict.get("slug"):
-    product_dict["slug"] = generate_slug(product_dict.get("title"))
+    image_path = await save_upload_file(image_url, "images")
 
-  new_product = Product(**product_dict, image_url=image_path, categories=categories)
-  session.add(new_product)
-  await session.commit()
-  return new_product
+    categories = []
+    if data.category_ids:
+        category_stmt = select(Category).where(Category.id.in_(data.category_ids))
+        category_result = await session.execute(category_stmt)
+        categories = category_result.scalars().all()
+
+    product_dict = data.model_dump(exclude={"category_ids"})
+    if not product_dict.get("slug"):
+        product_dict["slug"] = generate_slug(product_dict.get("title"))
+
+    new_product = Product(**product_dict, image_url=image_path, categories=categories)
+    session.add(new_product)
+    await session.commit()
+    return new_product
+
 
 async def get_all_products(
     session: AsyncSession,
     category_names: list[str] | None = None,
     limit: int = 5,
-    page: int = 1
+    page: int = 1,
 ) -> dict:
-  stmt = select(Product).options(selectinload(Product.categories))
+    stmt = select(Product).options(selectinload(Product.categories))
 
-  if category_names:
-    stmt = stmt.join(Product.categories).where(Category.name.in_(category_names)).distinct()
+    if category_names:
+        stmt = (
+            stmt.join(Product.categories)
+            .where(Category.name.in_(category_names))
+            .distinct()
+        )
 
-  count_stmt = stmt.with_only_columns(func.count(Product.id)).order_by(None)
-  total = await session.scalar(count_stmt)
+    count_stmt = stmt.with_only_columns(func.count(Product.id)).order_by(None)
+    total = await session.scalar(count_stmt)
 
-  stmt = stmt.limit(limit).offset((page-1)*limit)
+    stmt = stmt.limit(limit).offset((page - 1) * limit)
 
-  result = await session.execute(stmt)
-  products = result.scalars().all()
+    result = await session.execute(stmt)
+    products = result.scalars().all()
 
-  return {
-    "total": total,
-    "page": page,
-    "limit": limit,
-    "items": products
-  }
+    return {"total": total, "page": page, "limit": limit, "items": products}
+
 
 async def get_product_by_slug(session: AsyncSession, slug: str) -> ProductOut | None:
-  stmt = select(Product).options(selectinload(Product.categories)).where(Product.slug == slug)
-  result = await session.execute(stmt)
-  return result.scalar()
+    stmt = (
+        select(Product)
+        .options(selectinload(Product.categories))
+        .where(Product.slug == slug)
+    )
+    result = await session.execute(stmt)
+    return result.scalar()
+
 
 async def search_products(
     session: AsyncSession,
@@ -94,70 +114,76 @@ async def search_products(
     min_price: float | None = None,
     max_price: float | None = None,
     limit: int = 5,
-    page: int = 1
+    page: int = 1,
 ) -> dict:
-  stmt = select(Product).options(selectinload(Product.categories))
+    stmt = select(Product).options(selectinload(Product.categories))
 
-  if category_names:
-    stmt = stmt.join(Product.categories).where(Category.name.in_(category_names)).distinct()
+    if category_names:
+        stmt = (
+            stmt.join(Product.categories)
+            .where(Category.name.in_(category_names))
+            .distinct()
+        )
 
-  filters = []
+    filters = []
 
-  if title:
-    filters.append(Product.title.like(f"%{title}%"))
+    if title:
+        filters.append(Product.title.like(f"%{title}%"))
 
-  if description:
-    filters.append(Product.description.like(f"%{description}%"))
+    if description:
+        filters.append(Product.description.like(f"%{description}%"))
 
-  if min_price is not None:
-    filters.append(Product.price >= min_price)
+    if min_price is not None:
+        filters.append(Product.price >= min_price)
 
-  if max_price is not None:
-    filters.append(Product.price <= max_price)
+    if max_price is not None:
+        filters.append(Product.price <= max_price)
 
-  if filters:
-    stmt = stmt.where(and_(*filters))
+    if filters:
+        stmt = stmt.where(and_(*filters))
 
-  count_stmt = stmt.with_only_columns(func.count(Product.id)).order_by(None)
-  total = await session.scalar(count_stmt)
+    count_stmt = stmt.with_only_columns(func.count(Product.id)).order_by(None)
+    total = await session.scalar(count_stmt)
 
-  stmt = stmt.limit(limit).offset((page-1)*limit)
+    stmt = stmt.limit(limit).offset((page - 1) * limit)
 
-  result = await session.execute(stmt)
-  products = result.scalars().all()
+    result = await session.execute(stmt)
+    products = result.scalars().all()
 
-  return {
-    "total": total,
-    "page": page,
-    "limit": limit,
-    "items": products
-  }
+    return {"total": total, "page": page, "limit": limit, "items": products}
+
 
 async def update_product_by_id(
     session: AsyncSession,
     product_id: int,
     data: ProductUpdate,
-    image_url: UploadFile | None = None
+    image_url: UploadFile | None = None,
 ) -> ProductOut:
-  stmt = select(Product).options(selectinload(Product.categories)).where(Product.id == product_id)
-  result = await session.execute(stmt)
-  product = result.scalar_one_or_none()
+    stmt = (
+        select(Product)
+        .options(selectinload(Product.categories))
+        .where(Product.id == product_id)
+    )
+    result = await session.execute(stmt)
+    product = result.scalar_one_or_none()
 
-  if not product:
-    return None
-  
-  if data.category_ids is not None:
-    category_stmt = select(Category).where(Category.id.in_(data.category_ids))
-    category_result = await session.execute(category_stmt)
-    product.categories = category_result.scalars().all()
+    if not product:
+        return None
 
-  for key, value in data.model_dump(exclude={"category_id"}, exclude_none=True).items():
-    setattr(product, key, value)
+    if data.category_ids is not None:
+        category_stmt = select(Category).where(Category.id.in_(data.category_ids))
+        category_result = await session.execute(category_stmt)
+        product.categories = category_result.scalars().all()
 
-  if image_url is not None:
-    image_path = await save_upload_file(image_url, "images")
-    product.image_url = image_path
+    for key, value in data.model_dump(
+        exclude={"category_id"}, exclude_none=True
+    ).items():
+        setattr(product, key, value)
 
-  await session.commit()
-  await session.refresh(product)
-  return product
+    if image_url is not None:
+        image_path = await save_upload_file(image_url, "images")
+        product.image_url = image_path
+
+    await session.commit()
+    await session.refresh(product)
+    return product
