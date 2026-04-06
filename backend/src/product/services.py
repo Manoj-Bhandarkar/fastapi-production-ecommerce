e28f28b794a46a638ac6119326f65d0e3720d2c5
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-06 22:23:17 +0530

    Product - Product Update - Service - update_product_by_id created

--- backend/src/product/services.py
+++ backend/src/product/services.py
@@ -1,5 +1,5 @@
 from fastapi import HTTPException, UploadFile, status
-from sqlalchemy import select, func
+from sqlalchemy import select, func, and_
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.product.utils import generate_slug, save_upload_file
@@ -132,3 +132,32 @@ async def search_products(
     "limit": limit,
     "items": products
   }
+
+async def update_product_by_id(
+    session: AsyncSession,
+    product_id: int,
+    data: ProductUpdate,
+    image_url: UploadFile | None = None
+) -> ProductOut:
+  stmt = select(Product).options(selectinload(Product.categories)).where(Product.id == product_id)
+  result = await session.execute(stmt)
+  product = result.scalar_one_or_none()
+
+  if not product:
+    return None
+  
+  if data.category_ids is not None:
+    category_stmt = select(Category).where(Category.id.in_(data.category_ids))
+    category_result = await session.execute(category_stmt)
+    product.categories = category_result.scalars().all()
+
+  for key, value in data.model_dump(exclude={"category_id"}, exclude_none=True).items():
+    setattr(product, key, value)
+
+  if image_url is not None:
+    image_path = await save_upload_file(image_url, "images")
+    product.image_url = image_path
+
+  await session.commit()
+  await session.refresh(product)
+  return product
