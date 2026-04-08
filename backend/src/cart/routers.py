440ac5f34ca33d960567a5e1ce657fdeca5e1a25
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-08 10:30:01 +0530

    Cart - List User Cart - list_user_cart_item route is created

--- backend/src/cart/routers.py
+++ backend/src/cart/routers.py
@@ -2,15 +2,21 @@ from fastapi import APIRouter, Depends
 from src.account.deps import get_current_user
 from src.account.models import User
 from src.db.config import SessionDep
-from src.cart.schemas import CartItemCreate, CartItemOut
-from src.cart.services import add_to_cart
+from src.cart.schemas import CartItemCreate, CartItemOut, CartSummary
+from src.cart.services import add_to_cart, list_user_cart
 
 router = APIRouter()
 
+
 @router.post("/add", response_model=CartItemOut)
 async def add_item_to_cart(
-  session: SessionDep,
-  item:CartItemCreate,
-  user: User = Depends(get_current_user)
+    session: SessionDep, item: CartItemCreate, user: User = Depends(get_current_user)
 ):
-  return await add_to_cart(session, item, user.id)
+    return await add_to_cart(session, item, user.id)
+
+
+@router.get("", response_model=CartSummary)
+async def list_user_cart_item(
+    session: SessionDep, user: User = Depends(get_current_user)
+):
+    return await list_user_cart(session, user.id)
