964d84bf6c3f488cf881732c03377e48836263ba
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-09 21:42:49 +0530

    Cart - Increase and Decrease Cart Item -  increase_quantity_by_product route created

--- backend/src/cart/routers.py
+++ backend/src/cart/routers.py
@@ -3,7 +3,7 @@ from src.account.deps import get_current_user
 from src.account.models import User
 from src.db.config import SessionDep
 from src.cart.schemas import CartItemCreate, CartItemOut, CartSummary
-from src.cart.services import add_to_cart, list_user_cart
+from src.cart.services import add_to_cart, change_cart_item_quantity_by_product, list_user_cart
 
 router = APIRouter()
 
@@ -20,3 +20,11 @@ async def list_user_cart_item(
     session: SessionDep, user: User = Depends(get_current_user)
 ):
     return await list_user_cart(session, user.id)
+
+@router.patch("/increase/{product_id}", response_model=CartItemOut)
+async def increase_quantity_by_product(
+  session: SessionDep,
+  product_id: int,
+  user: User = Depends(get_current_user)
+):
+  return await change_cart_item_quantity_by_product(session, product_id, user.id, delta=1)
