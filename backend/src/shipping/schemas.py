3592b82db5d6095cc93bdff8c4e1363e3889d6fa
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-11 06:07:02 +0530

    Shipping - Shipping Address Schema created

---
+++ backend/src/shipping/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class ShippingAddressBase(BaseModel):
+    name: str
+    address_line1: str
+    address_line2: str | None = None
+    city: str
+    state: str
+    pin_code: str
+    country: str
+
+class ShippingAddressCreate(ShippingAddressBase):
+    pass
+
+class ShippingAddressOut(ShippingAddressBase):
+    id: int
+    user_id: int
+    model_config = {"from_attributes": True}
