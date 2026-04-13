43b53673e01f467456fbc85cfe596d19ea86f4f6
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-13 16:33:13 +0530

    Shipping - List Shipping Address - list_user_shipping_addresses funtion creates

--- backend/src/shipping/services.py
+++ backend/src/shipping/services.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.shipping.schemas import ShippingAddressCreate, ShippingAddressOut
 from src.shipping.models import ShippingAddress
@@ -13,3 +14,11 @@ async def create_shipping_address(
   await session.commit()
   await session.refresh(address)
   return address
+
+async def list_user_shipping_addresses(
+    session: AsyncSession,
+    user_id: int
+) -> list[ShippingAddressOut]:
+  stmt = select(ShippingAddress).where(ShippingAddress.user_id == user_id)
+  result = await session.execute(stmt)
+  return result.scalars().all()
