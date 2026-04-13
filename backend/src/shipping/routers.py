70887b70025d73677bd91c5c9090e5dcde20063c
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-13 16:37:24 +0530

    Shipping - Get User Shipping Address By Address ID - shipping_address_user_by_address_id route is creates

--- backend/src/shipping/routers.py
+++ backend/src/shipping/routers.py
@@ -3,7 +3,7 @@ from src.db.config import SessionDep
 from src.account.models import User
 from src.account.deps import get_current_user
 from src.shipping.schemas import ShippingAddressOut, ShippingAddressCreate
-from src.shipping.services import create_shipping_address, list_user_shipping_addresses
+from src.shipping.services import create_shipping_address, get_user_shipping_address_by_address_id, list_user_shipping_addresses
 
 
 router = APIRouter()
@@ -22,3 +22,11 @@ async def shipping_addresses_user_list(
   user: User = Depends(get_current_user)
 ):
   return await list_user_shipping_addresses(session, user.id)
+
+@router.get("/addresses/{address_id}", response_model=ShippingAddressOut)
+async def shipping_address_user_by_address_id(
+  session: SessionDep,
+  address_id: int,
+  user: User = Depends(get_current_user)
+):
+  return await get_user_shipping_address_by_address_id(session, address_id, user.id)
