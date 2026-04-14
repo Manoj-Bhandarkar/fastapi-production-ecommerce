d70498ffbda8cdd84d75b6936fb2a79676a72a51
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-14 23:08:48 +0530

    fix

--- backend/src/shipping/services.py
+++ backend/src/shipping/services.py
@@ -6,30 +6,30 @@ from src.shipping.models import ShippingAddress
 
 
 async def create_shipping_address(
-    session: AsyncSession,
-    user_id: int,
-    data: ShippingAddressCreate
+    session: AsyncSession, user_id: int, data: ShippingAddressCreate
 ) -> ShippingAddressOut:
-  address = ShippingAddress(user_id=user_id, **data.model_dump())
-  session.add(address)
-  await session.commit()
-  await session.refresh(address)
-  return address
+    address = ShippingAddress(user_id=user_id, **data.model_dump())
+    session.add(address)
+    await session.commit()
+    await session.refresh(address)
+    return address
+
 
 async def list_user_shipping_addresses(
-    session: AsyncSession,
-    user_id: int
+    session: AsyncSession, user_id: int
 ) -> list[ShippingAddressOut]:
-  stmt = select(ShippingAddress).where(ShippingAddress.user_id == user_id)
-  result = await session.execute(stmt)
-  return result.scalars().all()
+    stmt = select(ShippingAddress).where(ShippingAddress.user_id == user_id)
+    result = await session.execute(stmt)
+    return result.scalars().all()
+
 
 async def get_user_shipping_address_by_address_id(
-  session: AsyncSession,
-  address_id: int,
-  user_id: int
+    session: AsyncSession, address_id: int, user_id: int
 ) -> ShippingAddressOut:
-  address = await session.get(ShippingAddress, address_id)
-  if not address or address.user_id != user_id:
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Address not found or not authorized")
-  return address
+    address = await session.get(ShippingAddress, address_id)
+    if not address or address.user_id != user_id:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Address not found or not authorized",
+        )
+    return address
