59b16e857b00739b4b7a7e2b3737cd70a64da43e
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-15 20:33:52 +0530

    Shipping - Update User Shipping Address By Address ID - user_shipping_address_update_by_address_id route is created

--- backend/src/shipping/routers.py
+++ backend/src/shipping/routers.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter, Depends
 from src.db.config import SessionDep
 from src.account.models import User
 from src.account.deps import get_current_user
-from src.shipping.schemas import ShippingAddressOut, ShippingAddressCreate
+from src.shipping.schemas import ShippingAddressOut, ShippingAddressCreate, ShippingAddressUpdate
 from src.shipping.services import (
     create_shipping_address,
     get_user_shipping_address_by_address_id,
     list_user_shipping_addresses,
+    update_user_shipping_address_by_address_id,
 )
 
 
@@ -34,3 +35,12 @@ async def shipping_address_user_by_address_id(
     session: SessionDep, address_id: int, user: User = Depends(get_current_user)
 ):
     return await get_user_shipping_address_by_address_id(session, address_id, user.id)
+
+@router.patch("/addresses/{address_id}", response_model=ShippingAddressOut)
+async def user_shipping_address_update_by_address_id(
+  session: SessionDep,
+  address_id: int,
+  data: ShippingAddressUpdate,
+  user: User = Depends(get_current_user)
+):
+  return await update_user_shipping_address_by_address_id(session, address_id, user.id, data)
