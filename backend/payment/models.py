bce2aa049b7cbe58ca8d228353c93f5a364e9bed
Manoj-Bhandarkar <developer.manojbhandarkar@gmail.com>
2026-04-19 16:33:44 +0530

    Payment - Payment models is create

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -1,4 +1,9 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Boolean, ForeignKey, Enum
+from src.db.base import Base
+
 from enum import Enum as PyEnum
+
 
 class PaymentStatusEnum(str, PyEnum):
   pending = "pending"
@@ -9,3 +14,14 @@ class PaymentStatusEnum(str, PyEnum):
 class PaymentGatewayEnum(str, PyEnum):
   mock = "mock"
   razorpay = "razorpay"
+
+class Payment(Base):
+    __tablename__ = "payments"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id", ondelete="CASCADE"), nullable=False, unique=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    amount: Mapped[int] = mapped_column(nullable=False)
+    status: Mapped[PaymentStatusEnum] = mapped_column(Enum(PaymentStatusEnum), default=PaymentStatusEnum.pending, nullable=False)
+    payment_gateway: Mapped[PaymentGatewayEnum] = mapped_column(Enum(PaymentGatewayEnum), default=PaymentGatewayEnum.mock, nullable=False)
+    is_paid: Mapped[bool] = mapped_column(Boolean, default=False)
